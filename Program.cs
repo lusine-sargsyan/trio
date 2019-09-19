58c00686fbaa8ca84c4241cf206aa2f614c5f837
lusine <lusine.sargsyan71@outlook.com>
2019-09-19 18:15:40 +0400

    second

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace trio 
+{
+    class Program 
+    {
+        static void Main (string[] args) {
+            int a;
+            Console.WriteLine ("=====trio=====");
+            Console.WriteLine ("a: ");
+            a = Int32.Parse (Console.ReadLine ());
+            if (a % 2 == 1) 
+            {
+                for (int i = 1; i<= a; i += 2) 
+                {for (int k=1; k<=(a-i)/2; k++)
+                {Console.Write( " ");
+                }
+                    for (int j = 1; j<= i; j++) 
+                    {
+                        Console.Write ("*");
+                    }
+                    Console.WriteLine ();
+                }
+
+            }
+            else 
+            {Console.WriteLine("new namber");}
+        }
+    }
+}
